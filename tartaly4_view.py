393b66deedf1c41a5564c2519b1bc6be56a51dd7
Doszpod Andor <doszpod.andor@gmail.com>
2022-02-17 15:31:21 +0100

    create tartaly4 color refresh

--- tartaly4_view.py
+++ tartaly4_view.py
@@ -76,31 +76,126 @@ class App(PLC_View):
         super().loop()
 
     def start_refresh(self):
-        pass
+        if self.plc_data.start:
+            self.indicators.start.change_color('green')
+        else:
+            self.indicators.start.change_color('gray')
 
     def stop_refresh(self):
-        pass
+        if self.plc_data.stop:
+            self.indicators.stop.change_color('red')
+        else:
+            self.indicators.stop.change_color('gray')
 
     def dump_refresh(self):
-        pass
+        if self.plc_data.stert_urit:
+            self.indicators.start_urit.change_color('green')
+        else:
+            self.indicators.start_urit.change_color('gray')
 
     def tank1_refresh(self):
-        pass
+        # 1 1
+        if self.plc_data.t1_teli and self.plc_data.t1_fut:
+            self.tanks.tank1_change_color(sensor_color='red', heating_color='red')
+        # 0 1
+        elif not self.plc_data.t1_teli and self.plc_data.t1_fut:
+            self.tanks.tank1_change_color(sensor_color='gray', heating_color='red')
+        # 1 0
+        elif self.plc_data.t1_teli and not self.plc_data.t1_fut:
+            self.tanks.tank1_change_color(sensor_color='red', heating_color='black')
+        # 0 0
+        else:
+            self.tanks.tank1_change_color(sensor_color='gray', heating_color='black')
 
     def tank1_valve_refresh(self):
-        pass
+        # 1 1
+        if self.plc_data.t1_tolt and self.plc_data.t1_urit:
+            self.tanks.tank1_change_valve_color(top_valve_color='blue', bottom_valve_color='blue')
+        # 0 1
+        elif not self.plc_data.t1_tolt and self.plc_data.t1_urit:
+            self.tanks.tank1_change_valve_color(top_valve_color='gray', bottom_valve_color='blue')
+        # 1 0
+        elif self.plc_data.t1_tolt and not self.plc_data.t1_urit:
+            self.tanks.tank1_change_valve_color(top_valve_color='blue', bottom_valve_color='gray')
+        # 0 0
+        else:
+            self.tanks.tank1_change_valve_color(top_valve_color='gray', bottom_valve_color='gray')
 
     def tank2_refresh(self):
-        pass
+        # 1 1
+        if self.plc_data.t2_teli and self.plc_data.t2_fut:
+            self.tanks.tank2_change_color(sensor_color='red', heating_color='red')
+        # 0 1
+        elif not self.plc_data.t2_teli and self.plc_data.t2_fut:
+            self.tanks.tank2_change_color(sensor_color='gray', heating_color='red')
+        # 1 0
+        elif self.plc_data.t2_teli and not self.plc_data.t2_fut:
+            self.tanks.tank2_change_color(sensor_color='red', heating_color='black')
+        # 0 0
+        else:
+            self.tanks.tank2_change_color(sensor_color='gray', heating_color='black')
 
     def tank2_valve_refresh(self):
-        pass
+        # 1 1
+        if self.plc_data.t2_tolt and self.plc_data.t2_urit:
+            self.tanks.tank2_change_valve_color(top_valve_color='blue', bottom_valve_color='blue')
+        # 0 1
+        elif not self.plc_data.t2_tolt and self.plc_data.t2_urit:
+            self.tanks.tank1_change_valve_color(top_valve_color='gray', bottom_valve_color='blue')
+        # 1 0
+        elif self.plc_data.t2_tolt and not self.plc_data.t2_urit:
+            self.tanks.tank2_change_valve_color(top_valve_color='blue', bottom_valve_color='gray')
+        # 0 0
+        else:
+            self.tanks.tank2_change_valve_color(top_valve_color='gray', bottom_valve_color='gray')
 
     def tank3_refresh(self):
-        pass
+        # 1 1
+        if self.plc_data.t3_kever and self.plc_data.t3_urit:
+            self.tanks.tank3_change_color(rotor_color='green', bottom_valve_color='blue')
+        # 0 1
+        elif not self.plc_data.t3_kever and self.plc_data.t3_urit:
+            self.tanks.tank3_change_color(rotor_color='yellow', bottom_valve_color='blue')
+        # 1 0
+        elif self.plc_data.t3_kever and not self.plc_data.t3_urit:
+            self.tanks.tank3_change_color(rotor_color='green', bottom_valve_color='gray')
+        # 0 0
+        else:
+            self.tanks.tank3_change_color(rotor_color='yellow', bottom_valve_color='gray')
 
     def tank3_sensor_refresh(self):
-        pass
+        # 1 1 1
+        if self.plc_data.t3_felso and self.plc_data.t3_kozep and self.plc_data.t3_also:
+            self.tanks.tank3_change_sensor_color(
+                top_sensor_color='red', half_sensor_color='red', bottom_sensor_color='red')
+        # 0 1 1
+        elif not self.plc_data.t3_felso and self.plc_data.t3_kozep and self.plc_data.t3_also:
+            self.tanks.tank3_change_sensor_color(
+                top_sensor_color='gray', half_sensor_color='red', bottom_sensor_color='red')
+        # 1 0 1
+        elif self.plc_data.t3_felso and not self.plc_data.t3_kozep and self.plc_data.t3_also:
+            self.tanks.tank3_change_sensor_color(
+                top_sensor_color='red', half_sensor_color='gray', bottom_sensor_color='red')
+        # 0 0 1
+        elif not self.plc_data.t3_felso and not self.plc_data.t3_kozep and self.plc_data.t3_also:
+            self.tanks.tank3_change_sensor_color(
+                top_sensor_color='gray', half_sensor_color='gray', bottom_sensor_color='red')
+        # 1 1 0
+        elif self.plc_data.t3_felso and self.plc_data.t3_kozep and not self.plc_data.t3_also:
+            self.tanks.tank3_change_sensor_color(
+                top_sensor_color='red', half_sensor_color='red', bottom_sensor_color='gray')
+        # 0 1 0
+        elif not self.plc_data.t3_felso and self.plc_data.t3_kozep and not self.plc_data.t3_also:
+            self.tanks.tank3_change_sensor_color(
+                top_sensor_color='gray', half_sensor_color='red', bottom_sensor_color='gray')
+        # 1 0 0
+        elif self.plc_data.t3_felso and not self.plc_data.t3_kozep and not self.plc_data.t3_also:
+            self.tanks.tank3_change_sensor_color(
+                top_sensor_color='red', half_sensor_color='gray', bottom_sensor_color='gray')
+        # 0 0 0
+        else:
+            self.tanks.tank3_change_sensor_color(
+                top_sensor_color='gray', half_sensor_color='gray', bottom_sensor_color='gray')
 
 
 if __name__ == '__main__':
