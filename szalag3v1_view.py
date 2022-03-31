d1fbd0b69c4d893294458d41c1c21500806d8096
Andor Doszpod <doszpod.andor@gmail.com>
2022-04-01 00:53:11 +0200

    create szalag 3v1

---
+++ szalag3v1_view.py
@@ -0,0 +1,195 @@
+from _view.plc_viewa import PLC_ViewA
+from szalag_data.szalag3v1_data import Szalag3_data
+from szalag_data.szalag3v1_draw import Szalag3_View
+
+
+class App(PLC_ViewA):
+
+    # noinspection PyPep8Naming
+    def __init__(self, screenName=None, baseName=None, className='Tk', useTk=True, sync=False, use=None):
+        super().__init__(screenName, baseName, className, useTk, sync, use)
+
+        # noinspection SpellCheckingInspection
+        self.title('Szalag 3v1')
+
+        # noinspection SpellCheckingInspection
+        self.name_label.config(text='Szalag 3v1')
+
+        self.conveyors = Szalag3_View(self.process_frame)
+        self.conveyors.pack()
+
+        self.plc_data = Szalag3_data(self.ip_select.ip_address.get(), self.plc_rack, self.plc_slot)
+
+    def loop(self):
+        super().loop()
+
+        if self.plc_data.uzem_is_changed() or self.plc_data.szalag_hiba_is_changed() or self.plc_data.kocsi_hiba_is_changed():
+            self.indicator_refresh()
+
+        if self.plc_data.silo1_ures_is_changed() or self.plc_data.silo2_ures_is_changed():
+            self.silo_indicator_refresh()
+
+        if self.plc_data.m1_is_changed() or self.plc_data.s1_is_changed():
+            self.silo1_refresh()
+
+        if self.plc_data.m2_is_changed() or self.plc_data.s2_is_changed():
+            self.silo2_refresh()
+
+        if self.plc_data.m3_is_changed() or self.plc_data.s3_is_changed():
+            self.conveyor1_refresh()
+
+        if self.plc_data.m4_is_changed() or self.plc_data.s4_is_changed():
+            self.conveyor2_refresh()
+
+        if self.plc_data.m5_is_changed() or self.plc_data.s5_is_changed():
+            self.conveyor3_refresh()
+
+        if self.plc_data.kp_is_changed() or self.plc_data.kp_is_changed():
+            self.wagon_refresh()
+
+        if self.plc_data.ks_is_changed(threshold=1000):
+            self.wagon_wight_refresh()
+
+    def indicator_refresh(self):
+
+        # 1 1 1
+        if self.plc_data.uzem and self.plc_data.szalag_hiba and self.plc_data.kocsi_hiba:
+            self.conveyors.indicator_change_color(factory_color='green',
+                                                  conveyor_error_color='red',
+                                                  wagon_error_color='red')
+        # 0 1 1
+        elif not self.plc_data.uzem and self.plc_data.szalag_hiba and self.plc_data.kocsi_hiba:
+            self.conveyors.indicator_change_color(factory_color='gray',
+                                                  conveyor_error_color='red',
+                                                  wagon_error_color='red')
+        # 1 0 1
+        elif self.plc_data.uzem and not self.plc_data.szalag_hiba and self.plc_data.kocsi_hiba:
+            self.conveyors.indicator_change_color(factory_color='green',
+                                                  conveyor_error_color='gray',
+                                                  wagon_error_color='red')
+        # 0 0 1
+        elif not self.plc_data.uzem and not self.plc_data.szalag_hiba and self.plc_data.kocsi_hiba:
+            self.conveyors.indicator_change_color(factory_color='gray',
+                                                  conveyor_error_color='gray',
+                                                  wagon_error_color='red')
+        # 1 1 0
+        elif self.plc_data.uzem and self.plc_data.szalag_hiba and not self.plc_data.kocsi_hiba:
+            self.conveyors.indicator_change_color(factory_color='green',
+                                                  conveyor_error_color='red',
+                                                  wagon_error_color='gray')
+        # 0 1 0
+        elif not self.plc_data.uzem and self.plc_data.szalag_hiba and not self.plc_data.kocsi_hiba:
+            self.conveyors.indicator_change_color(factory_color='gray',
+                                                  conveyor_error_color='red',
+                                                  wagon_error_color='gray')
+        # 1 0 0
+        elif self.plc_data.uzem and not self.plc_data.szalag_hiba and not self.plc_data.kocsi_hiba:
+            self.conveyors.indicator_change_color(factory_color='green',
+                                                  conveyor_error_color='gray',
+                                                  wagon_error_color='gray')
+        # 0 0 0
+        else:
+            self.conveyors.indicator_change_color(factory_color='gray',
+                                                  conveyor_error_color='gray',
+                                                  wagon_error_color='gray')
+
+    def silo_indicator_refresh(self):
+        # 1 1
+        if self.plc_data.silo1_ures and self.plc_data.silo2_ures:
+            self.conveyors.silo_indicator_change_color(silo1_empty_color='red', silo2_empty_color='red')
+        # 0 1
+        elif not self.plc_data.silo1_ures and self.plc_data.silo2_ures:
+            self.conveyors.silo_indicator_change_color(silo1_empty_color='gray', silo2_empty_color='red')
+        # 1 0
+        elif self.plc_data.silo1_ures and not self.plc_data.silo2_ures:
+            self.conveyors.silo_indicator_change_color(silo1_empty_color='red', silo2_empty_color='gray')
+        # 0 0
+        else:
+            self.conveyors.silo_indicator_change_color(silo1_empty_color='gray', silo2_empty_color='gray')
+
+    def silo1_refresh(self):
+        # 1 1
+        if self.plc_data.m1 and self.plc_data.s1:
+            self.conveyors.silo1_change_color(motor_color='green', sensor_color='red')
+        # 0 1
+        elif not self.plc_data.m1 and self.plc_data.s1:
+            self.conveyors.silo1_change_color(motor_color='gray', sensor_color='red')
+        # 1 0
+        elif self.plc_data.m1 and not self.plc_data.s1:
+            self.conveyors.silo1_change_color(motor_color='green', sensor_color='gray')
+        else:
+            self.conveyors.silo1_change_color(motor_color='gray', sensor_color='gray')
+
+    def silo2_refresh(self):
+        # 1 1
+        if self.plc_data.m2 and self.plc_data.s2:
+            self.conveyors.silo2_change_color(motor_color='green', sensor_color='red')
+        # 0 1
+        elif not self.plc_data.m2 and self.plc_data.s2:
+            self.conveyors.silo2_change_color(motor_color='gray', sensor_color='red')
+        # 1 0
+        elif self.plc_data.m2 and not self.plc_data.s2:
+            self.conveyors.silo2_change_color(motor_color='green', sensor_color='gray')
+        else:
+            self.conveyors.silo2_change_color(motor_color='gray', sensor_color='gray')
+
+    def conveyor1_refresh(self):
+        # 1 1
+        if self.plc_data.m3 and self.plc_data.s3:
+            self.conveyors.conveyor1_change_color(motor_color='green', sensor_color='green')
+        # 0 1
+        elif not self.plc_data.m3 and self.plc_data.s3:
+            self.conveyors.conveyor1_change_color(motor_color='gray', sensor_color='yellow')
+        # 1 0
+        elif self.plc_data.m3 and not self.plc_data.s3:
+            self.conveyors.conveyor1_change_color(motor_color='green', sensor_color='red')
+        # 0 0
+        else:
+            self.conveyors.conveyor1_change_color(motor_color='grey', sensor_color='grey')
+
+    def conveyor2_refresh(self):
+        # 1 1
+        if self.plc_data.m4 and self.plc_data.s4:
+            self.conveyors.conveyor2_change_color(motor_color='green', sensor_color='green')
+        # 0 1
+        elif not self.plc_data.m4 and self.plc_data.s4:
+            self.conveyors.conveyor2_change_color(motor_color='gray', sensor_color='yellow')
+        # 1 0
+        elif self.plc_data.m4 and not self.plc_data.s4:
+            self.conveyors.conveyor2_change_color(motor_color='green', sensor_color='red')
+        # 0 0
+        else:
+            self.conveyors.conveyor2_change_color(motor_color='grey', sensor_color='grey')
+
+    def conveyor3_refresh(self):
+        # 1 1
+        if self.plc_data.m5 and self.plc_data.s5:
+            self.conveyors.conveyor3_change_color(motor_color='green', sensor_color='green')
+        # 0 1
+        elif not self.plc_data.m5 and self.plc_data.s5:
+            self.conveyors.conveyor3_change_color(motor_color='gray', sensor_color='yellow')
+        # 1 0
+        elif self.plc_data.m5 and not self.plc_data.s5:
+            self.conveyors.conveyor3_change_color(motor_color='green', sensor_color='red')
+        # 0 0
+        else:
+            self.conveyors.conveyor3_change_color(motor_color='grey', sensor_color='grey')
+
+    def wagon_refresh(self):
+        # 1
+        if self.plc_data.kp:
+            self.conveyors.wagon_change_color(sensor_color='red')
+        # 0
+        else:
+            self.conveyors.wagon_change_color(sensor_color='gray')
+
+    def wagon_wight_refresh(self):
+        self.conveyors.wagon_wight_change(wight_percent=self.plc_data.ks_percent)
+
+
+if __name__ == '__main__':
+    app = App()
+
+    app.after(100, app.loop)
+
+    app.mainloop()
---
+++ szalag_data/szalag3v1_data.py
@@ -0,0 +1,302 @@
+from _plc_data.plc_data import PLC_Address, PLC_data
+
+
+class Szalag3_Address(PLC_Address):
+
+    S1 = 'I0.0'
+    S2 = 'I0.1'
+    S3 = 'I0.2'
+    S4 = 'I0.3'
+    S5 = 'I0.4'
+    KP = 'I0.5'
+    START = 'I0.6'
+    STOP = 'I0.7'
+
+    M1 = 'Q0.0'
+    M2 = 'Q0.1'
+    M3 = 'Q0.2'
+    M4 = 'Q0.3'
+    M5 = 'Q0.4'
+    SZALAG_HIBA = 'Q0.5'
+    KOCSI_HOBA = 'Q4.0'
+    UZEM = 'Q4.1'
+    SILO1_URES = 'Q4.2'
+    SILO2_URES = 'Q4.3'
+
+    READ_BYTES_ADDRESS = (('IB0', 1), ('QB0', 5))
+
+    KS = 'IW64'
+
+    READ_WORDS_ADDRESS = (('IW64', 1), )
+
+    KS_RANGE = 27648
+
+
+class Szalag3_data(PLC_data):
+
+    __s1_old = False
+    __s2_old = False
+    __s3_old = False
+    __s4_old = False
+    __s5_old = False
+    __kp_old = False
+    __start_old = False
+    __stop_old = False
+
+    __m1_old = False
+    __m2_old = False
+    __m3_old = False
+    __m4_old = False
+    __m5_old = False
+    __szalag_hiba_old = False
+    __kocsi_hiba_old = False
+    __uzem_old = False
+    __silo1_ures_old = False
+    __silo2_ures_old = False
+
+    __ks_old = 0
+
+    def __init__(self, ip, rack, slot) -> None:
+        super().__init__(Szalag3_Address(), ip, rack, slot)
+
+        self.__s1 = False
+        self.__s2 = False
+        self.__s3 = False
+        self.__s4 = False
+        self.__s5 = False
+        self.__kp = False
+        self.__start = False
+        self.__stop = False
+
+        self.__m1 = False
+        self.__m2 = False
+        self.__m3 = False
+        self.__m4 = False
+        self.__m5 = False
+        self.__szalag_hiba = False
+        self.__kocsi_hiba = False
+        self.__uzem = False
+        self.__silo1_ures = False
+        self.__silo2_ures = False
+
+        self.__ks = 0
+
+    @property
+    def s1(self):
+        return self.__s1
+
+    @property
+    def s2(self):
+        return self.__s2
+
+    @property
+    def s3(self):
+        return self.__s3
+
+    @property
+    def s4(self):
+        return self.__s4
+
+    @property
+    def s5(self):
+        return self.__s5
+
+    @property
+    def kp(self):
+        return self.__kp
+
+    @property
+    def start(self):
+        return self.__start
+
+    @property
+    def stop(self):
+        return self.__stop
+
+    @property
+    def m1(self):
+        return self.__m1
+
+    @property
+    def m2(self):
+        return self.__m2
+
+    @property
+    def m3(self):
+        return self.__m3
+
+    @property
+    def m4(self):
+        return self.__m4
+
+    @property
+    def m5(self):
+        return self.__m5
+
+    @property
+    def szalag_hiba(self):
+        return self.__szalag_hiba
+
+    @property
+    def kocsi_hiba(self):
+        return self.__kocsi_hiba
+
+    @property
+    def uzem(self):
+        return self.__uzem
+
+    @property
+    def silo1_ures(self):
+        return self.__silo1_ures
+
+    @property
+    def silo2_ures(self):
+        return self.__silo2_ures
+
+    @property
+    def ks(self):
+        return self.__ks
+
+    @property
+    def ks_percent(self):
+        return int(self.__ks / Szalag3_Address.KS_RANGE * 100)
+
+    def read_data(self) -> None:
+        super().read_data()
+
+        self.__s1 = self.get_bit_in_page(Szalag3_Address.S1)
+        self.__s2 = self.get_bit_in_page(Szalag3_Address.S2)
+        self.__s3 = self.get_bit_in_page(Szalag3_Address.S3)
+        self.__s4 = self.get_bit_in_page(Szalag3_Address.S4)
+        self.__s5 = self.get_bit_in_page(Szalag3_Address.S5)
+        self.__kp = self.get_bit_in_page(Szalag3_Address.KP)
+        self.__start = self.get_bit_in_page(Szalag3_Address.START)
+        self.__stop = self.get_bit_in_page(Szalag3_Address.STOP)
+
+        self.__m1 = self.get_bit_in_page(Szalag3_Address.M1)
+        self.__m2 = self.get_bit_in_page(Szalag3_Address.M2)
+        self.__m3 = self.get_bit_in_page(Szalag3_Address.M3)
+        self.__m4 = self.get_bit_in_page(Szalag3_Address.M4)
+        self.__m5 = self.get_bit_in_page(Szalag3_Address.M5)
+        self.__szalag_hiba = self.get_bit_in_page(Szalag3_Address.SZALAG_HIBA)
+        self.__kocsi_hiba = self.get_bit_in_page(Szalag3_Address.KOCSI_HOBA)
+        self.__uzem = self.get_bit_in_page(Szalag3_Address.UZEM)
+        self.__silo1_ures = self.get_bit_in_page(Szalag3_Address.SILO1_URES)
+        self.__silo2_ures = self.get_bit_in_page(Szalag3_Address.SILO2_URES)
+
+        self.__ks = self.get_int_in_page(Szalag3_Address.KS)
+
+    def s1_is_changed(self):
+        if self.__s1 != self.__s1_old:
+            self.__s1_old = self.__s1
+            return True
+        return False
+
+    def s2_is_changed(self):
+        if self.__s2 != self.__s2_old:
+            self.__s2_old = self.__s2
+            return True
+        return False
+
+    def s3_is_changed(self):
+        if self.__s3 != self.__s3_old:
+            self.__s3_old = self.__s3
+            return True
+        return False
+
+    def s4_is_changed(self):
+        if self.__s4 != self.__s4_old:
+            self.__s4_old = self.__s4
+            return True
+        return False
+
+    def s5_is_changed(self):
+        if self.__s5 != self.__s5_old:
+            self.__s5_old = self.__s5
+            return True
+        return False
+
+    def kp_is_changed(self):
+        if self.__kp != self.__kp_old:
+            self.__kp_old = self.__kp
+            return True
+        return False
+
+    def start_is_changed(self):
+        if self.__start != self.__start_old:
+            self.__start_old = self.__start
+            return True
+        return False
+
+    def stop_is_changed(self):
+        if self.__stop != self.__stop_old:
+            self.__stop_old = self.__stop
+            return True
+        return False
+
+    def m1_is_changed(self):
+        if self.__m1 != self.__m1_old:
+            self.__m1_old = self.__m1
+            return True
+        return False
+
+    def m2_is_changed(self):
+        if self.__m2 != self.__m2_old:
+            self.__m2_old = self.__m2
+            return True
+        return False
+
+    def m3_is_changed(self):
+        if self.__m3 != self.__m3_old:
+            self.__m3_old = self.__m3
+            return True
+        return False
+
+    def m4_is_changed(self):
+        if self.__m4 != self.__m4_old:
+            self.__m4_old = self.__m4
+            return True
+        return False
+
+    def m5_is_changed(self):
+        if self.__m5 != self.__m5_old:
+            self.__m5_old = self.__m5
+            return True
+        return False
+
+    def szalag_hiba_is_changed(self):
+        if self.__szalag_hiba != self.__szalag_hiba_old:
+            self.__szalag_hiba_old = self.__szalag_hiba
+            return True
+        return False
+
+    def kocsi_hiba_is_changed(self):
+        if self.__kocsi_hiba != self.__kocsi_hiba_old:
+            self.__kocsi_hiba_old = self.__kocsi_hiba
+            return True
+        return False
+
+    def uzem_is_changed(self):
+        if self.__uzem != self.__uzem_old:
+            self.__uzem_old = self.__uzem
+            return True
+        return False
+
+    def silo1_ures_is_changed(self):
+        if self.__silo1_ures != self.__silo1_ures_old:
+            self.__silo1_ures_old = self.__silo1_ures
+            return True
+        return False
+
+    def silo2_ures_is_changed(self):
+        if self.__silo2_ures != self.__silo2_ures_old:
+            self.__silo2_ures_old = self.__silo2_ures
+            return True
+        return False
+
+    def ks_is_changed(self, threshold=0):
+        if self.__ks < self.__ks_old - threshold // 2 or \
+                self.__ks > self.__ks_old + threshold // 2:
+            self.__ks_old = self.__ks
+            return True
+        return False
