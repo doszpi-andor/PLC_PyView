54e3c10e292eeb915e3c779a9d4fbb608b8f6ea3
Andor Doszpod <doszpod.andor@gmail.com>
2023-04-12 22:57:56 +0200

    bugfix refresh method

--- mozgatas1A_view.py
+++ mozgatas1A_view.py
@@ -35,25 +35,25 @@ class App(PLC_ViewA):
 
     def sensor_refresh(self):
         # 1 1 1
-        if self.plc_data.e0 or self.plc_data.e1 or self.plc_data.e2:
+        if self.plc_data.e0 and self.plc_data.e1 and self.plc_data.e2:
             self.indicator.sensor_change_color(sensor0_color='green', sensor1_color='green', sensor2_color='green')
         # 0 1 1
-        elif not self.plc_data.e0 or self.plc_data.e1 or self.plc_data.e2:
+        elif not self.plc_data.e0 and self.plc_data.e1 and self.plc_data.e2:
             self.indicator.sensor_change_color(sensor0_color='gray', sensor1_color='green', sensor2_color='green')
         # 1 0 1
-        elif self.plc_data.e0 or not self.plc_data.e1 or self.plc_data.e2:
+        elif self.plc_data.e0 and not self.plc_data.e1 and self.plc_data.e2:
             self.indicator.sensor_change_color(sensor0_color='green', sensor1_color='gray', sensor2_color='green')
         # 0 0 1
-        elif not self.plc_data.e0 or not self.plc_data.e1 or self.plc_data.e2:
+        elif not self.plc_data.e0 and not self.plc_data.e1 and self.plc_data.e2:
             self.indicator.sensor_change_color(sensor0_color='gray', sensor1_color='gray', sensor2_color='green')
         # 1 1 0
-        elif self.plc_data.e0 or self.plc_data.e1 or not self.plc_data.e2:
+        elif self.plc_data.e0 and self.plc_data.e1 and not self.plc_data.e2:
             self.indicator.sensor_change_color(sensor0_color='green', sensor1_color='green', sensor2_color='gray')
         # 0 1 0
-        elif not self.plc_data.e0 or self.plc_data.e1 or not self.plc_data.e2:
+        elif not self.plc_data.e0 and self.plc_data.e1 and not self.plc_data.e2:
             self.indicator.sensor_change_color(sensor0_color='gray', sensor1_color='green', sensor2_color='gray')
         # 1 0 0
-        elif self.plc_data.e0 or not self.plc_data.e1 or not self.plc_data.e2:
+        elif self.plc_data.e0 and not self.plc_data.e1 and not self.plc_data.e2:
             self.indicator.sensor_change_color(sensor0_color='green', sensor1_color='gray', sensor2_color='gray')
         # 0 0 0
         else:
@@ -61,25 +61,25 @@ class App(PLC_ViewA):
 
     def indicator_refresh(self):
         # 1 1 1
-        if self.plc_data.m_fel or self.plc_data.m_fog or self.plc_data.m_le:
+        if self.plc_data.m_fel and self.plc_data.m_fog and self.plc_data.m_le:
             self.indicator.indicator_change_color(motor_up_color='red', motor_stack_color='red', motor_down_color='red')
         # 0 1 1
-        elif not self.plc_data.m_fel or self.plc_data.m_fog or self.plc_data.m_le:
+        elif not self.plc_data.m_fel and self.plc_data.m_fog and self.plc_data.m_le:
             self.indicator.indicator_change_color(motor_up_color='gray', motor_stack_color='red', motor_down_color='red')
         # 1 0 1
-        elif self.plc_data.m_fel or not self.plc_data.m_fog or self.plc_data.m_le:
+        elif self.plc_data.m_fel and not self.plc_data.m_fog and self.plc_data.m_le:
             self.indicator.indicator_change_color(motor_up_color='gray', motor_stack_color='red', motor_down_color='gray')
         # 0 0 1
-        elif not self.plc_data.m_fel or not self.plc_data.m_fog or self.plc_data.m_le:
+        elif not self.plc_data.m_fel and not self.plc_data.m_fog and self.plc_data.m_le:
             self.indicator.indicator_change_color(motor_up_color='gray', motor_stack_color='gray', motor_down_color='red')
         # 1 1 0
-        elif self.plc_data.m_fel or self.plc_data.m_fog or not self.plc_data.m_le:
+        elif self.plc_data.m_fel and self.plc_data.m_fog and not self.plc_data.m_le:
             self.indicator.indicator_change_color(motor_up_color='red', motor_stack_color='red', motor_down_color='gray')
         # 0 1 0
-        elif not self.plc_data.m_fel or self.plc_data.m_fog or not self.plc_data.m_le:
+        elif not self.plc_data.m_fel and self.plc_data.m_fog and not self.plc_data.m_le:
             self.indicator.indicator_change_color(motor_up_color='gray', motor_stack_color='red', motor_down_color='gray')
         # 1 0 0
-        elif self.plc_data.m_fel or not self.plc_data.m_fog or not self.plc_data.m_le:
+        elif self.plc_data.m_fel and not self.plc_data.m_fog and not self.plc_data.m_le:
             self.indicator.indicator_change_color(motor_up_color='red', motor_stack_color='gray', motor_down_color='gray')
         # 0 0 0
         else:
